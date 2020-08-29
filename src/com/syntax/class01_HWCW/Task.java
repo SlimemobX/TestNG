3ca28a4f2e9915f1e372011aa8047cdff02cba6e
SlimemobX <ferrufino2@gmail.comX>
2020-08-29 12:14:36 -0400

    TestNG Intro & Task

---
+++ src/com/syntax/class01_HWCW/Task.java
@@ -0,0 +1,22 @@
+package com.syntax.class01_HWCW;
+
+import org.testng.annotations.Test;
+
+public class Task {
+	
+	@Test
+	public void taskOne() {
+		System.out.println("Hello World");
+	}
+	
+	@Test
+	public void taskTwo() {
+		System.out.println("Welcome World");
+	}
+	
+	@Test
+	public void taskThree() {
+		System.out.println("Hi World");
+	}
+
+}
